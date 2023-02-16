a6cb57e4e8e485e7a17e1ba5cd28de99fe60b256
Rodrigo Cabrera <rocabrera1994@gmail.com>
2023-02-16 15:07:14 -0300

    first commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -32,32 +32,7 @@ function App() {
      
   
  
- /*
-   const[allPokemons, setAllPokemons] = useState([])
-   const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-  const getAllPokemons = async () => {
-    const res = await fetch(loadMore)
-    const data = await res.json()
-
-    setLoadMore(data.next)
-
-    function createPokemonObject(results)  {
-      results.forEach( async pokemon => {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
-        const data =  await res.json()
-        setAllPokemons( currentList => [...currentList, data])
-        
-      })
-    }
-    createPokemonObject(data.results)
-  }
-
- useEffect(() => {
-  getAllPokemons()
- }, [])
-
-  */
  
 
   return (
